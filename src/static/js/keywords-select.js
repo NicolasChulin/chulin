9f2c131046b278ddec938d39b826b54099b35049
zhangjie <chulinice@sina.com>
2018-06-13 08:18:17 +0800

    track and qmui test

--- src/static/js/keywords-select.js
+++ src/static/js/keywords-select.js
@@ -2,134 +2,150 @@
  * Created by zhangjie on 2016/5/3.
  */
 
+$(function() {
+  /*选择关键词*/
+  $("#ed-choose").on("click", ".ed-choose-li", function(e) {
+    //        e.preventDefault();
+    var ths = $(this);
+    var chebox = ths.children().children();
+    var cont = ths.attr("data-cont");
+    var view = ths.attr("data-view");
+    var id = ths.attr("data-id");
+    var edcont = $("#ed-content");
+    if (view == "0") {
+      var num = edcont.children().length;
+      if (num >= 2) {
+        alert("您选择的关键词已多余2个");
+        return;
+      }
+      ths.attr("data-view", "1");
+      chebox.removeClass("glyphicon-unchecked").addClass("glyphicon-check");
+      var txt =
+        '<span id="ed-con-li' +
+        id +
+        '" data-id="' +
+        id +
+        '">' +
+        cont +
+        "</span>";
+      edcont.append($(txt));
+    } else {
+      ths.attr("data-view", "0");
+      chebox.removeClass("glyphicon-check").addClass("glyphicon-unchecked");
+      $("#ed-con-li" + id).remove();
+    }
+  });
 
-$(function(){
-    /*选择关键词*/
-    $('#ed-choose').on('click','.ed-choose-li',function(e){
+  $("#ed-choose").on("click", ".glyphicon-minus", function(e) {
+    e.stopPropagation();
+    var ths = $(this);
+    var edchos = ths.parent();
+    var id = edchos.attr("data-id");
 
-//        e.preventDefault();
-        var ths = $(this);
-        var chebox = ths.children().children();
-        var cont = ths.attr('data-cont');
-        var view = ths.attr('data-view');
-        var id = ths.attr('data-id');
-        var edcont= $('#ed-content');
-        if(view == '0'){
-            var num = edcont.children().length;
-            if(num>=2){
-                alert('您选择的关键词已多余2个');
-                return;
-            }
-            ths.attr('data-view','1');
-            chebox.removeClass('glyphicon-unchecked').addClass('glyphicon-check');
-            var txt = '<span id="ed-con-li'+id+'" data-id="'+id+'">'+cont+'</span>';
-            edcont.append($(txt));
-        }else{
-            ths.attr('data-view','0');
-            chebox.removeClass('glyphicon-check').addClass('glyphicon-unchecked');
-            $('#ed-con-li'+id).remove();
-        }
+    //删除成功后的操作
+    edchos.remove();
+    $("#ed-con-li" + id).remove();
+  });
 
-    });
+  $("#ed-content").on("click", "span", function() {
+    var ths = $(this);
+    var id = ths.attr("data-id");
+    ths.remove();
+    $(".ed-choose-li")
+      .filter("[data-id=" + id + "]")
+      .addClass("here");
+  });
 
-    $('#ed-choose').on('click','.glyphicon-minus',function(e){
-        e.stopPropagation();
-        var ths = $(this);
-        var edchos = ths.parent();
-        var id = edchos.attr('data-id');
+  /*自定义添加关键词*/
+  $("#addItem").on("click", function() {
+    var newKwn = $("#newKwn").val();
+    var newKwv = $("#newKwv").val();
+    if (newKwv == "" || newKwn == "") {
+      alert("请填写必要的词汇");
+      return;
+    }
+    if (getCharLen(newKwn) > 50 || getCharLen(newKwv) > 50) {
+      alert("您填写的词汇过多");
+      return;
+    }
 
-        //删除成功后的操作
-        edchos.remove();
-        $('#ed-con-li'+id).remove();
-    });
+    //添加成功后的操作
+    var num = $(".ed-choose-li").length;
+    var id = num + 1;
+    var cht =
+      '<div class="ed-choose-li col-xs-6" data-cont="' +
+      newKwn +
+      newKwv +
+      '" data-view="1" data-id="' +
+      id +
+      '">';
+    cht += '<div class="cbox-line">';
+    cht += '<input type="checkbox" checked="checked">' + newKwn + "+" + newKwv;
+    cht +=
+      '</div><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></div>';
+    $("#ed-choose")
+      .children()
+      .append($(cht));
+    var txt =
+      '<span id="ed-con-li' +
+      id +
+      '" data-id="' +
+      id +
+      '">' +
+      newKwn +
+      newKwv +
+      "</span>";
+    $("#ed-content").append($(txt));
 
-    $('#ed-content').on('click','span',function(){
-        var ths = $(this);
-        var id = ths.attr('data-id');
-        ths.remove();
-        $('.ed-choose-li').filter('[data-id='+id+']').addClass('here');
+    $("#newKwn").prop("value", "");
+    $("#newKwv").prop("value", "");
+  });
 
-    });
+  /*提交选好的关键词*/
+  $("#submit").on("click", function() {
+    var edcont = $("#ed-content").children();
+    var num = edcont.length;
+    var name = "";
+    if (num == 0) {
+      yyAlert("请选择关键词");
+      return;
+    }
 
-    /*自定义添加关键词*/
-    $("#addItem").on('click',function(){
-
-        var newKwn = $('#newKwn').val();
-        var newKwv = $('#newKwv').val();
-        if(newKwv =='' ||newKwn == ''){
-            alert('请填写必要的词汇');
-            return;
-        }
-        if(getCharLen(newKwn)>50||getCharLen(newKwv)>50){
-            alert('您填写的词汇过多');
-            return;
-        }
-
-        //添加成功后的操作
-        var num = $('.ed-choose-li').length;
-        var id = num+1;
-        var cht = '<div class="ed-choose-li col-xs-6" data-cont="'+newKwn+newKwv+'" data-view="1" data-id="'+id+'">';
-            cht += '<div class="cbox-line">';
-            cht += '<input type="checkbox" checked="checked">'+newKwn+'+'+newKwv;
-            cht += '</div><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></div>';
-        $('#ed-choose').children().append($(cht));
-        var txt = '<span id="ed-con-li'+id+'" data-id="'+id+'">'+newKwn+newKwv+'</span>';
-        $('#ed-content').append($(txt));
-
-        $('#newKwn').prop('value','');
-        $('#newKwv').prop('value','');
-
-    });
-
-    /*提交选好的关键词*/
-    $("#submit").on('click',function(){
-        var edcont = $('#ed-content').children();
-        var num = edcont.length;
-        var name = '';
-        if(num == 0){
-            yyAlert('请选择关键词');
-            return;
-        }
-
-        for(var i=0;i<num;i++){
-            var kid= $(edcont.get(i)).attr('data-id');
-            if(name)  name += ','+kid;
-            else name += kid;
-        }
-        console.log(name);
-//        window.location.href='';
-
-    });
-    /*返回*/
-    $('#goback').on('click',function(){
-        var newKwn = $.trim($('#newKwn').val());
-        var newKwv = $.trim($('#newKwv').val());
-        var edcont = $('#ed-content').children();
-        var num = edcont.length;
-        if(newKwn != ''|| newKwv != '' || num!=0){
-            var conf= confirm('您当前有编辑过的内容，确定要退出吗？');
-            if(conf){
-               alert('11');
-            }
-        }
-        alert('11');
-    });
+    for (var i = 0; i < num; i++) {
+      var kid = $(edcont.get(i)).attr("data-id");
+      if (name) name += "," + kid;
+      else name += kid;
+    }
+    console.log(name);
+    //        window.location.href='';
+  });
+  /*返回*/
+  $("#goback").on("click", function() {
+    var newKwn = $.trim($("#newKwn").val());
+    var newKwv = $.trim($("#newKwv").val());
+    var edcont = $("#ed-content").children();
+    var num = edcont.length;
+    if (newKwn != "" || newKwv != "" || num != 0) {
+      var conf = confirm("您当前有编辑过的内容，确定要退出吗？");
+      if (conf) {
+        alert("11");
+      }
+    }
+    alert("11");
+  });
 });
 
-
-
-function getCharLen(char){
-    var leng=0;
-    var len = char.length;
-    if(len==0)return leng;
-    for(var i =0 ;i<len;i++){
-        var c =char.charCodeAt(i);
-        if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {
-            leng++;
-        }else{
-            leng+=2;
-        }
+function getCharLen(char) {
+  var leng = 0;
+  var len = char.length;
+  if (len == 0) return leng;
+  for (var i = 0; i < len; i++) {
+    var c = char.charCodeAt(i);
+    if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+      leng++;
+    } else {
+      leng += 2;
     }
-    return leng;
+  }
+  return leng;
 }
-
--- src/temps/multipage/track-face/js/tracking-extend.js
+++ src/temps/multipage/track-face/js/tracking-extend.js
@@ -4,11 +4,10 @@
  * @description extend tracking
  * @description created in 2018-05-23 13:50:00
  */
-(function (window, undefined) {
-
+(function(window, undefined) {
   tracking.trackVideo_ = function(element, tracker) {
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
+    var canvas = document.createElement("canvas");
+    var context = canvas.getContext("2d");
     var width;
     var height;
 
@@ -19,7 +18,7 @@
       canvas.height = height;
     };
     resizeCanvas_();
-    element.addEventListener('resize', resizeCanvas_);
+    element.addEventListener("resize", resizeCanvas_);
 
     var requestId;
     var start;
@@ -27,15 +26,13 @@
     var requestAnimationFrame_ = function() {
       requestId = window.requestAnimationFrame(function(timestamp) {
         if (element.readyState === element.HAVE_ENOUGH_DATA) {
-          // 目前展示注释掉，源代码在tracking.js line 252
-          // try {
-          //   context.drawImage(element, 0, 0, width, height);
-          // } catch (err) {}
-
           if (!start) start = timestamp;
           var during = timestamp - start;
           if (during > aniDuring) {
             start = timestamp;
+            try {
+              context.drawImage(element, 0, 0, width, height);
+            } catch (err) {}
             tracking.trackCanvasInternal_(canvas, tracker);
           }
         }
@@ -44,10 +41,10 @@
     };
 
     var task = new tracking.TrackerTask(tracker);
-    task.on('stop', function() {
+    task.on("stop", function() {
       window.cancelAnimationFrame(requestId);
     });
-    task.on('run', function() {
+    task.on("run", function() {
       requestAnimationFrame_();
     });
     return task.run();
@@ -75,35 +72,31 @@
   tracking.ObjectTracker.prototype.setAniDuring = function(aniDuring) {
     this.aniDuring = aniDuring;
   };
-
 })(window);
-
 
 /**
  * TrackingFace class for application
  */
-(function (window, undefined) {
-
+(function(window, undefined) {
   /**
    * @param       {[type]} opts.videoId 视频元素id，默认为vide
    * @param       {[type]} opts.canvasId canva元素id，默认为canvas
    * @param       {[type]} opts.aniDuring 视频捕捉间隔，默认2000 ms
    * @constructor
    */
-  function TrackingFace (opts) {
-
-    var opts = typeof opts === 'undefined' ? {} : opts;
+  function TrackingFace(opts) {
+    var opts = typeof opts === "undefined" ? {} : opts;
     this.stopT = 0;
     this.runT = 0;
     this.stopDuring = 3000;
     this.runDuring = 300;
-    this.videoId = opts.videoId || 'video';
-    this.canvasId = opts.canvasId || 'canvas';
+    this.videoId = opts.videoId || "video";
+    this.canvasId = opts.canvasId || "canvas";
     this.aniDuring = opts.aniDuring || 2000;
-    this.tracker =  new tracking.ObjectTracker('face');
-    this.trackTask = null
-    this.morePeopleWarning = null
-    this.nonePeopleWarning = null
+    this.tracker = new tracking.ObjectTracker("face");
+    this.trackTask = null;
+    this.morePeopleWarning = null;
+    this.nonePeopleWarning = null;
   }
 
   /**
@@ -111,105 +104,117 @@
    * @param  {[function]} morePeopleWarning [more people catched callback]
    * @param  {[function]} nonePeopleWarning [none people catched callback]
    */
-  TrackingFace.prototype.catchView = function (morePeopleWarning, nonePeopleWarning) {
-
+  TrackingFace.prototype.catchView = function(
+    morePeopleWarning,
+    nonePeopleWarning
+  ) {
     this.tracker.setInitialScale(4);
     this.tracker.setStepSize(2);
     this.tracker.setEdgesDensity(0.1);
     this.tracker.setAniDuring(this.aniDuring);
     this.morePeopleWarning = morePeopleWarning;
     this.nonePeopleWarning = nonePeopleWarning;
-    this.trackTask = tracking.track('#' + this.videoId, this.tracker, { camera: true });
-    this.bindEvent()
-  }
+    this.trackTask = tracking.track("#" + this.videoId, this.tracker, {
+      camera: true
+    });
+    this.bindEvent();
+  };
 
   /**
    * bind event
    */
-  TrackingFace.prototype.bindEvent = function () {
+  TrackingFace.prototype.bindEvent = function() {
     var _this = this;
     var canvas = document.getElementById(_this.canvasId);
     var videoDom = document.getElementById(_this.videoId);
-    var context = canvas.getContext('2d');
+    var context = canvas.getContext("2d");
 
-    _this.tracker.on('track', function (event) {
+    _this.tracker.on("track", function(event) {
       context.clearRect(0, 0, canvas.width, canvas.height);
 
       if (event.data.length > 1) {
         // 出现多人情况
         // 截取视频图像
         context.drawImage(videoDom, 0, 0, canvas.width, canvas.height);
-        var shotImg = canvas.toDataURL('image/jpeg', 0.5);
+        var shotImg = canvas.toDataURL("image/jpeg", 0.5);
         // 触发回调
-        if (typeof _this.morePeopleWarning === 'function') _this.morePeopleWarning(shotImg)
-
+        if (typeof _this.morePeopleWarning === "function")
+          _this.morePeopleWarning(shotImg);
       } else if (event.data.length === 0) {
         // 没有人情况-回调
-        if (typeof _this.nonePeopleWarning === 'function') _this.nonePeopleWarning()
+        if (typeof _this.nonePeopleWarning === "function")
+          _this.nonePeopleWarning();
       } else {
         // console.log('1111')
         // console.log(event.data)
       }
 
       // 绘制头像识别框
-      event.data.forEach(function (rect) {
-        context.strokeStyle = '#dd0a20';
+      event.data.forEach(function(rect) {
+        context.strokeStyle = "#dd0a20";
         context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.font = '11px Helvetica';
+        context.font = "11px Helvetica";
         context.fillStyle = "#dd0a20";
-        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+        context.fillText(
+          "x: " + rect.x + "px",
+          rect.x + rect.width + 5,
+          rect.y + 11
+        );
+        context.fillText(
+          "y: " + rect.y + "px",
+          rect.x + rect.width + 5,
+          rect.y + 22
+        );
       });
     });
-  }
+  };
 
   /**
    * 停止捕捉任务
    */
-  TrackingFace.prototype.stopCatch = function () {
+  TrackingFace.prototype.stopCatch = function() {
     this.trackTask.stop();
-  }
+  };
 
   /**
    * 开始捕捉任务
    */
-  TrackingFace.prototype.runCatch = function () {
+  TrackingFace.prototype.runCatch = function() {
     this.trackTask.run();
-  }
+  };
 
   // 周期阶段性捕捉最好不用
   // 周期性捕捉，每捕捉500毫秒，暂停2500毫秒
   /**
    * 开启周期停止
    */
-  TrackingFace.prototype.periodStopCatch = function () {
+  TrackingFace.prototype.periodStopCatch = function() {
     var _this = this;
     _this.stopCatch();
-    _this.stopT = setTimeout(function () {
-      _this.periodRunCatch()
-    }, _this.stopDuring)
-  }
+    _this.stopT = setTimeout(function() {
+      _this.periodRunCatch();
+    }, _this.stopDuring);
+  };
 
   /**
    * 开启周期运行
    */
-  TrackingFace.prototype.periodRunCatch = function () {
+  TrackingFace.prototype.periodRunCatch = function() {
     var _this = this;
-    _this.runCatch()
-    _this.runT = setTimeout(function () {
-      _this.periodStopCatch()
-    }, _this.runDuring)
-  }
+    _this.runCatch();
+    _this.runT = setTimeout(function() {
+      _this.periodStopCatch();
+    }, _this.runDuring);
+  };
 
   /**
    * 清除周期性捕捉
    */
-  TrackingFace.prototype.clearPeriod = function () {
+  TrackingFace.prototype.clearPeriod = function() {
     clearTimeout(this.stopT);
     clearTimeout(this.runT);
-    this.stopCatch()
-  }
+    this.stopCatch();
+  };
 
   window.TrackingFace = TrackingFace;
-
 })(window);
